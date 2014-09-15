05b49ddc2d90c7e635077b57f2d3b5f9b9682d83
Swooper <bluecloak@gmail.com>
2014-09-15 00:36:56 +0000

    Hexgrid now scales to screen size
    Cell size no longer hard-coded, now calculated based on screen size.
    Game now supports different board sizes.

--- src/ru/appdev/HexFlow/HexBoard.java
+++ src/ru/appdev/HexFlow/HexBoard.java
@@ -1,8 +1,10 @@
 package ru.appdev.HexFlow;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.*;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import java.util.List;
@@ -25,12 +27,22 @@ public class HexBoard extends View {
     public HexBoard(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
 
-        m_cellWidth = 20;
-        m_cellHeight = Math.sqrt(3)* (m_cellWidth /2); // This is the proportion between width and height in a hexagon.
+        // The following block of code is from Stackoverflow, specifically
+        // http://stackoverflow.com/questions/19155559/how-to-get-android-device-screen-size and
+        // http://stackoverflow.com/questions/8833825/error-getting-window-size-on-android-the-method-getwindowmanager-is-undefined
+        DisplayMetrics dm = new DisplayMetrics();
+        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int screenWidth = dm.widthPixels;
+
+        m_cellWidth = ((screenWidth - getPaddingLeft() - getPaddingRight()) / (2*BOARD_SIZE-0.66f)) * 4/3;
+        // This may look weird but there's reasons.
+        // The *4/3 has to do with the way the hex grid is arranged - each column overlaps the previous one by 1/4.
+        // -0.66f instead of -1 is to avoid clipping off the right corners and edges of the last column.
+        m_cellHeight = Math.sqrt(3)* (m_cellWidth /2); // This is the ratio between width and height in a hexagon.
         x_cellShift = 3* (float) m_cellWidth /4; // The x-axis offset between hex columns
         y_cellShift = (float) m_cellHeight /2; // The y-axis offset between hex columns
 
-        m_paintGrid.setColor(Color.parseColor("#666666"));
+        m_paintGrid.setColor(Color.parseColor("#666666")); // Hardcoded colour until I can figure out how to use /res/values files properly.
     }
 
     @Override
@@ -51,6 +63,8 @@ public class HexBoard extends View {
         canvas.drawPath( m_path, m_paintPath);*/
     }
 
+    // Draws a whole grid of hexes, as determined by BOARD_SIZE.
+    // Calls the drawHex method to draw each individual hex.
     private void drawHexGrid(Canvas canvas) {
         int columns = BOARD_SIZE*2-1;
         int rows = BOARD_SIZE*2-1;
@@ -60,13 +74,14 @@ public class HexBoard extends View {
                 float yij;
                 if (2 * BOARD_SIZE > j + Math.abs(BOARD_SIZE - i)) { //Filtering out hexes we don't want to draw
                     yij = j * (float) m_cellHeight + Math.abs(BOARD_SIZE - i) * y_cellShift;
-
-                    drawHex(i * x_cellShift, yij, 10.0f, canvas);
+                    drawHex((i-1) * x_cellShift, yij, canvas);
                 }
             }
         }
 
     }
+
+    // Draws a single hex.
     // x and y input coordinates are the top left corner of
     // the square bounding the hex.
     //  Corners of the hex:
@@ -74,29 +89,39 @@ public class HexBoard extends View {
     // A /   \ D
     //   \___/
     //  F    E
-    private void drawHex(float x, float y, float scale, Canvas canvas) {
+    // TODO get rid of scale variable
+    private void drawHex(float x, float y, Canvas canvas) {
 
         float h = (float) m_cellHeight;
         float w = (float) m_cellWidth;
         //A -> B
-        canvas.drawLine( scale*x, scale*(y+h/2), scale*(x+w/4), scale*y, m_paintGrid);
+        canvas.drawLine( x, y+h/2, x+w/4, y, m_paintGrid);
         //B -> C
-        canvas.drawLine( scale*(x + w/4), scale*y, scale*(x+3*w/4), scale*y, m_paintGrid);
+        canvas.drawLine( x + w/4, y, x+3*w/4, y, m_paintGrid);
         //C -> D
-        canvas.drawLine( scale*(x+3*w/4), scale*y, scale*(x+w), scale*(y+h/2), m_paintGrid);
+        canvas.drawLine( x+3*w/4, y, x+w, y+h/2, m_paintGrid);
         //D -> E
-        canvas.drawLine( scale*(x+w), scale*(y+h/2), scale*(x+3*w/4), scale*(y+h), m_paintGrid);
+        canvas.drawLine( x+w, y+h/2, x+3*w/4, y+h, m_paintGrid);
         //E -> F
-        canvas.drawLine( scale*(x+3*w/4), scale*(y+h), scale*(x+w/4), scale*(y+h), m_paintGrid);
+        canvas.drawLine( x+3*w/4, y+h, x+w/4, y+h, m_paintGrid);
         //F -> A
-        canvas.drawLine( scale*(x+w/4), scale*(y+h), scale*x, scale*(y+h/2), m_paintGrid);
+        canvas.drawLine( x+w/4, y+h, x, y+h/2, m_paintGrid);
     }
+
+    // Simplified - the border between columns is a straight line in this game.
     private int xToCol( int x ) {
-        return (int) (x  / m_cellWidth);
+        int col;
+        col = (int) ((x - m_cellWidth/8) / (3 / 4 * m_cellWidth));
+        return col;
     }
-    private int yToRow( int y ) {
-        return (int) (y  / m_cellHeight);
+    private int yToRow( int y , int x ) {
+        int col = xToCol(x);
+        int row;
+        row = (int)( y-((m_cellHeight/2)*Math.abs(BOARD_SIZE - col)/ m_cellHeight));
+        return row;
     }
+
+    // TODO fix these two methods when I add the code that uses them.
     private int colToX( int col ) {
         return (int) (col * m_cellWidth);
     }
@@ -108,4 +133,25 @@ public class HexBoard extends View {
         m_paintPath.setColor( color );
         invalidate();
     }
+    /**
+    @Override
+    protected void onSizeChanged( int xNew, int yNew, int xOld, int yOld ) {
+        int sw = Math.max(1, (int) m_paintGrid.getStrokeWidth());
+        m_cellWidth = (xNew - getPaddingLeft() - getPaddingRight() - sw) / ((2 * BOARD_SIZE) - 1);
+        m_cellHeight = Math.sqrt(3)* (m_cellWidth /2); // Proportions between width and height in a hexagon.
+        x_cellShift = 3* (float) m_cellWidth /4; // The x-axis offset between hex columns
+        y_cellShift = (float) m_cellHeight /2; // The y-axis offset between hex columns
+
+        //m_cellHeight = (yNew - getPaddingTop() - getPaddingBottom() - sw) / (2*BOARD_SIZE-1);
+    }
+
+    @Override
+    protected void onMeasure( int widthMeasureSpec, int heightMeasureSpec ) {
+        super.onMeasure( widthMeasureSpec, heightMeasureSpec );
+        int width = getMeasuredWidth() - getPaddingLeft() - getPaddingRight();
+        int height = getMeasuredHeight() - getPaddingTop() - getPaddingBottom();
+        int size = Math.min(width, height);
+        setMeasuredDimension( size + getPaddingLeft() + getPaddingRight(),
+            size + getPaddingTop() + getPaddingBottom() );
+    }*/
 }
